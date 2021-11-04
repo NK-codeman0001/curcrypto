00659e8f8153bab2340b02854f39cb161e2c8368
Neeraj Kumar <66253834+NK-codeman0001@users.noreply.github.com>
2021-11-04 14:40:49 +0530

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-        'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-        'x-rapidapi-key': '8e0564a26amshacfebcef6b00395p1b299ajsn0c65a2b02ea2'
+  'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+  'x-rapidapi-host': process.env.CRYPTOAPI_HOST,
 };
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://coinranking1.p.rapidapi.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.RCRYPTOAPI_URL  }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
